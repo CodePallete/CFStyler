517c97b6020fbba4e7a79b15fad95f82cb5c95cf
sak13301 <sak13301@esri.com>
2026-08-19 09:35:08 +0530

    added test ui for dom injection

---
+++ src/components/codeforcesui/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./index.module.css";
+
+export default function CodeforcesUi() {
+  return (
+    <div className={styles.main}>
+      <strong className={styles.title}>CF Styler</strong>
+      <p>React content UI injected successfully.</p>
+    </div>
+  );
+}
